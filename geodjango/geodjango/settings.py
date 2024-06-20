efa48a16bce33ae4b579b0b392a7d7dd8d7ca5ee
hoareausylvie <hoareausms@gmail.com>
2024-06-20 13:57:24 +0400

    Update setting.py

--- geodjango/geodjango/settings.py
+++ geodjango/geodjango/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
+    "world"
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,18 @@ WSGI_APPLICATION = "geodjango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": BASE_DIR / "geodjango",
+        "USER": "geo"
     }
 }
 
